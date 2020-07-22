1885827251a623b3331ac35b73e6826cafb2a3e3
Ken Lauguico <kenlauguico@Kens-iMac.local>
2020-07-22 15:03:25 -0700

    added dash/start endpoint

--- api/dash/controllers/dash.js
+++ api/dash/controllers/dash.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { sanitizeEntity } = require('strapi-utils');
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
@@ -7,13 +9,25 @@
 
 module.exports = {
     async create(ctx) {
-        if (ctx.request.body[0].title) {
+        let entity, response
+        if (ctx.request.body.length) {
             const dashes = ctx.request.body
-            const links = ctx.request.body[0].items
-            dashes.map(dash => { console.log('Add dash '+dash.title) })
-            links.map(link => { console.log('Add '+link.title) })
-            return ctx.request.body[0].items
-        }        
-        return ctx.request.body // await strapi.services.dash.find(ctx.query);
+            for (var i in dashes) {
+                let linkIds = [], dash = dashes[i]
+                const links = dashes[i].links
+                for (var x in links) {
+                    let entity = await strapi.services.link.create(links[x]);
+                    let { id } = sanitizeEntity(entity, { model: strapi.models.link })
+                    linkIds.push(id)
+                }
+                dash.links = linkIds
+                entity = await strapi.services.dash.create(dash);
+                response = sanitizeEntity(entity, { model: strapi.models.dash })
+            }
+        }
+        if (!response) {
+            return ctx.unauthorized('Nope')
+        }
+        return response
     }
 };
